6e06808099a7d771dcb3b44cc876c150ad274f39
Amirhosein Torabi <torabi46@gmail.com>
2024-09-07 09:21:04 +0330

    initial setup for cache api

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,10 @@
 self.addEventListener("install", function (event) {
-    console.log("[service worker] Install service worke ...", event)
 });
 self.addEventListener("activate", function (event) {
-    console.log("[service worker] Activation service worker ...", event)
     event.waitUntil(self.clients.claim());
     // return self.clients.claim();
 });
 
-self.addEventListener("fetch", function (event) {
-    console.log(event)
-    console.log("fetch event", (event.request));
+self.addEventListener(" fetch", function (event) {
     // console.log("[service worker] fetching something...", event);
 })
